52a24ef5b9da42ec4449ebd61fd6688455d79514
Tanisha <tani@tanisha.com>
2023-07-29 22:56:26 +0530

    updated

--- index.mjs
+++ index.mjs
@@ -6,12 +6,18 @@ import requests from "requests";
 const homeFile=fs.readFileSync("home.html","utf-8");
 
 const replaceVal=(tempVal,orgVal)=>{
-     let temperature=tempVal.replace("{%tempval%}", orgVal.main.temp);
-     temperature=temperature.replace("{%tempmin%}", orgVal.main.temp_min);
-     temperature=temperature.replace("{%tempmax%}", orgVal.main.temp_max);
-     temperature=temperature.replace("{%location%}", orgVal.name);
-     temperature=temperature.replace("{%country%}", orgVal.sys.country);
-     temperature=temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+    //  let temperature=tempVal.replace("{%tempval%}", orgVal.main.temp);
+    //  temperature=temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    //  temperature=temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    //  temperature=temperature.replace("{%location%}", orgVal.name);
+    //  temperature=temperature.replace("{%country%}", orgVal.sys.country);
+    //  temperature=temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+    let temperature = tempVal.replace('{%tempval%}', (Math.round((orgVal.main.temp-273.15)*100)/100).toFixed(2));
+    temperature = temperature.replace('{%tempmin%}', (Math.round((orgVal.main.temp_min-273.15)*100)/100).toFixed(2));
+    temperature = temperature.replace('{%tempmax%}', (Math.round((orgVal.main.temp_max-273.15)*100)/100).toFixed(2));
+    temperature = temperature.replace('{%location%}', orgVal.name);
+    temperature = temperature.replace('{%country%}', orgVal.sys.country);
+    temperature = temperature.replace('{%tempStatus%}', orgVal.weather[0].main);
      return temperature;
 }
 const server=http.createServer((req,res)=>{
@@ -32,8 +38,11 @@ const server=http.createServer((req,res)=>{
   if (err) return console.log('connection closed due to errors', err);
   res.end();
  
-  console.log('end');
+ 
 })
+    }
+    else{
+      res.end("file not found");
     }
 });
 server.listen(8000,"127.0.0.1");
